58f0ce8db651516c455b2419a29dea20591f4f82
sudarshanatidvaya <sudarshanmise@atidvaya.com>
2016-09-03 04:25:51 +0530

    last from system

---
+++ TestWeb/TestAWebAPI/TestAWebAPI/Controllers/NewsFeedController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestAWebAPI.Models;
+
+namespace TestAWebAPI.Controllers
+{
+    [RoutePrefix("api/News")]
+    public class NewsFeedController : ApiController
+    {
+        [Route("Get/{1}")]
+        public NewsFeed Get(int id)
+        {
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            
+            SqlCommand cmd = new SqlCommand("p_GetNewsFeedById", myConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("@Id", SqlDbType.Int).Value = id;
+            
+            NewsFeed newsFeed = new NewsFeed();
+            try
+            {
+                myConnection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    newsFeed = new NewsFeed();
+                    newsFeed.ID = Convert.ToInt32(reader.GetValue(0));
+                    newsFeed.UserProfileName = reader.GetValue(1).ToString();
+                    newsFeed.UserProfilePicID = reader.GetValue(2).ToString();
+                    newsFeed.StatusMessage = reader.GetValue(3).ToString();
+                    newsFeed.NewsImage = reader.GetValue(4).ToString();
+                    newsFeed.LinkText = reader.GetValue(5).ToString();
+                    newsFeed.TimeStamp = Convert.ToDateTime(reader.GetValue(6)).Ticks.ToString();
+                    newsFeed.IsActive = Convert.ToBoolean(reader.GetValue(7));
+                }
+                return newsFeed;
+            }
+            catch (Exception ex)
+            {
+                return new NewsFeed();
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+        }
+
+
+        [HttpPost]
+        [Route("PostNewsFeed")]
+        public void Post(NewsFeed newsFeed)
+        {
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            newsFeed = new NewsFeed(2, "admin2", "some Image ID", "user", "some imageid again", DateTime.Now.ToString(), "ola ola ola", "linkkk", "imageeee", true);
+            
+            
+
+            SqlCommand cmd = new SqlCommand("p_SaveNewsFeed", myConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("@UserName", SqlDbType.NVarChar).Value = newsFeed.UserProfileName;
+            cmd.Parameters.Add("@UserImageID", SqlDbType.NVarChar).Value = newsFeed.UserProfilePicID;
+            cmd.Parameters.Add("@StatusMessage", SqlDbType.NVarChar).Value = newsFeed.StatusMessage;
+            cmd.Parameters.Add("@StatusImageID", SqlDbType.NVarChar).Value = newsFeed.NewsImage;
+            cmd.Parameters.Add("@StatusLink", SqlDbType.NVarChar).Value = newsFeed.LinkText;
+            cmd.Parameters.Add("@StatusDateTime", SqlDbType.DateTime).Value = newsFeed.TimeStamp;
+            cmd.Parameters.Add("@IsActive", SqlDbType.Bit).Value = newsFeed.IsActive;
+
+            try
+            {
+                myConnection.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
---
+++ TestWeb/TestAWebAPI/TestAWebAPI/Models/NewsFeed.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAWebAPI.Models
+{
+    public class NewsFeed
+    {
+        int _id;
+        string _userProfileName;
+        string _loggedInUserProfileName;
+        string _userProfilePicID;
+        string _loggedInUserProfilePicID;
+        string _timeStamp;
+        string _statusMessage;
+        string _linkText;
+        string _newsImage;
+        bool _isActive;
+
+
+        public NewsFeed() { }
+
+        public NewsFeed(int id, string userProfileName, string userProfilePicID, string loggedInUserProfileName, string loggedInUserProfilePicID,
+            string timeStamp, string statusMessage, string linkText, string newsImage, bool isActive)
+        {
+            _id = id;
+            _userProfileName = userProfileName;
+            _loggedInUserProfileName = loggedInUserProfileName;
+            _userProfilePicID = userProfilePicID;
+            _loggedInUserProfilePicID = loggedInUserProfilePicID;
+            _timeStamp = timeStamp;
+            _statusMessage = statusMessage;
+            _linkText = linkText;
+            _newsImage = newsImage;
+            _isActive = isActive;
+        }
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string UserProfileName
+        {
+            get { return _userProfileName; }
+            set { _userProfileName = value; }
+        }
+
+        public string UserProfilePicID
+        {
+            get { return _userProfilePicID; }
+            set { _userProfilePicID = value; }
+        }
+
+        public string LoggedInUserProfileName
+        {
+            get { return _loggedInUserProfileName; }
+            set { _loggedInUserProfileName = value; }
+        }
+
+        public string LoggedInUserProfilePicID
+        {
+            get { return _loggedInUserProfilePicID; }
+            set { _loggedInUserProfilePicID = value; }
+        }
+
+        public string TimeStamp
+        {
+            get { return _timeStamp; }
+            set { _timeStamp = value; }
+        }
+
+        public string StatusMessage
+        {
+            get { return _statusMessage; }
+            set { _statusMessage = value; }
+        }
+
+        public string LinkText
+        {
+            get { return _linkText; }
+            set { _linkText = value; }
+        }
+
+        public string NewsImage
+        {
+            get { return _newsImage; }
+            set { _newsImage = value; }
+        }
+
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set { _isActive = value; }
+        }
+    }
+}
